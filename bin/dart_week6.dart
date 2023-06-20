b327607b7cbc570295a065f3e8ad84d01b65ae6d
sarin013 <dawnrun0147@gmail.com>
2023-06-20 13:09:52 +0700

    first commit

---
+++ bin/dart_week6.dart
@@ -0,0 +1,10 @@
+void main(){
+  int x =10;
+  double y = 5;
+  print("Arithemetic x + y: $x + $y = ${x + y}");//1.Addition
+  print("Arithemetic x - y: $x - $y = ${x - y}");//2.Subtraction
+  print("Arithemetic x * y: $x * $y = ${x * y}");//3.Multiply
+  print("Arithemetic x / y: $x / $y = ${x / y}");//4.Division
+  print("Arithemetic x ~/ y: $x ~/ $y = ${x ~/ y}");//5.Divisionm retuning integer
+  print("Arithemetic x % y: $x % $y = ${x % y}");//6.Modulus
+}
